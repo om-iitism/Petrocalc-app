253eedb8245009e72a20861b7a317cc773deeb80
om-iitism <22dr0155@iitism.ac.in>
2025-04-07 01:29:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.subheader("Calculate Apparent Water Resistivity (Rwa) and Water Saturation (S
 porosity_percent = st.slider("Porosity (%)", min_value=0.0, max_value=35.0, value=15.0, step=0.1)
 
 # Input: Total Resistivity (Rt) - Logarithmic scale
-rt_log = st.slider("Total Resistivity (Rt, Ohm·m)", min_value=0.2, max_value=200, value=15.0, step=0.1)
+rt_log = st.slider("Total Resistivity (Rt, Ohm·m)", min_value=0.2, max_value=200, step=0.1)
 rt = 10 ** rt_log
 
 # Input: Formation Water Resistivity (Rw)
